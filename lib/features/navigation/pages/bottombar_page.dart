d25abe694732e8894a6d23880fe3a120ab763ffc
huyle <levanhuy2k4@gmail.com>
2025-11-19 14:38:52 +0700

    update button setting

--- lib/features/navigation/pages/bottombar_page.dart
+++ lib/features/navigation/pages/bottombar_page.dart
@@ -13,6 +13,8 @@ import 'package:fintrack/features/home/bloc/home_bloc.dart';
 import 'package:fintrack/features/home/pages/home_page.dart';
 import 'package:fintrack/features/navigation/bloc/bottom_bloc.dart';
 import 'package:fintrack/features/navigation/pages/bottom_nav_item.dart';
+import 'package:fintrack/features/setting/presentation/bloc/setting_bloc.dart';
+import 'package:fintrack/features/setting/presentation/pages/setting_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -49,7 +51,10 @@ class _BottombarPageState extends State<BottombarPage> {
       // ),
       SignUpPage(),
       BlocProvider(create: (context) => HomeBloc(), child: HomePage()),
-      BlocProvider(create: (context) => HomeBloc(), child: HomePage()),
+      BlocProvider(
+        create: (_) => sl<SettingBloc>()..add(LoadSettingCardsEvent()),
+        child: const SettingPage(),
+      ),
     ];
 
     return BlocProvider(
